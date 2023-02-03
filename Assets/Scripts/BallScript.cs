708b0fdaa670262b6bcdc6fccd271412a041a6b4
Luis Núñez <panicjoker03@outlook.com>
2023-02-02 22:17:00 -0600

    Ball mechanics
    Ball gameplay enchancement. 
    Loose live and ball reset mechanics.

--- Assets/Scripts/BallScript.cs
+++ Assets/Scripts/BallScript.cs
@@ -9,6 +9,13 @@ public class BallScript : MonoBehaviour
     private Vector2 direction;
     private Rigidbody2D rigi_bdy;
 
+    private bool ballIsActive = false;
+    private Vector2 ballPosition;
+    private Vector2 ballInitialForce;
+    private float originY;
+    //referencia para que pelota siga al palo
+    public GameObject playerObj;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,13 +24,48 @@ public class BallScript : MonoBehaviour
         direction.x = 1.0f;
         rigi_bdy = GetComponent<Rigidbody2D>();
         //rigi_bdy.AddForce(direction * speedForce);
-        rigi_bdy.velocity = direction * speedForce;
+        //fuerza
+        ballInitialForce = direction * speedForce;
+        ballIsActive = false;
+        ballPosition = transform.position;
+        //para saber en que 'y' inició la pelota
+        originY = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //eyectar bola
+        if(Input.GetButtonDown("Jump") == true)
+        {
+            if(!ballIsActive)
+            {
+                rigi_bdy.velocity = ballInitialForce;
+                ballIsActive = true;
+            }
+        }
+
+        //mover pelota junto a palo
+        if(!ballIsActive && playerObj != null)
+        {
+            ballPosition.x = playerObj.transform.position.x;
+            transform.position = ballPosition;
+        }
+
+        //todo: loose live 
+        if(ballIsActive == true && transform.position.y < -6)
+        {
+            ballIsActive = false;
+            ballPosition.x = playerObj.transform.position.x;
+            ballPosition.y = originY;
+            transform.position = ballPosition;
+
+            //perder vida y luego revisar si es game over
+            var wall = GameObject.Find("Walls");
+            wall.SendMessage("TakeLive");
+            wall.SendMessage("WinLose");
+        }
+            
     }
 
     void OnCollisionEnter2D(Collision2D coll)
@@ -49,20 +91,27 @@ public class BallScript : MonoBehaviour
             var new_direction = rigi_bdy.velocity;
             //game play from arkanoid...
             float player_offset = player_rgd_bdy.transform.position.x - transform.position.x;
-            new_direction.x += player_rgd_bdy.velocity.x / 8 + player_offset / 2;
+            new_direction.x += player_rgd_bdy.velocity.x / 6.0f - player_offset * 5.3f;
+            //decrease x speed a little... im lazy about correcting the vector angle
+            new_direction.x *= 0.8f;
             //minimal y speed when hitting paddle
-            if(new_direction.y < 0.4)
-                new_direction.y = 0.4f; 
+            if(new_direction.y < 1.3)
+                new_direction.y = 1.3f; 
+            
+            //tood: preserve a minimal speed***
+            //if(new_direction.magnitude 
+
             rigi_bdy.velocity = new_direction;
         }
 
         if(collid_g_obj.tag == "Wall")
         {
             //prevent wall horizontal lock
-            if(Mathf.Abs(rigi_bdy.velocity.x) < 0.1)
+            if(Mathf.Abs(rigi_bdy.velocity.y) < 0.35f)
             {
+                Debug.Log("Aquí se atoró la bola");
                 var new_direction = rigi_bdy.velocity;
-                new_direction.y += Mathf.Sign(rigi_bdy.velocity.y) * 0.1f;
+                new_direction.y = Mathf.Sign(rigi_bdy.velocity.y) * 1.24f;
                 rigi_bdy.velocity = new_direction;
             }
         }
